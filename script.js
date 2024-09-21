98384964491128717283c867db03520735abab9d
valentina <vs4153588@gmail.com>
2024-09-21 12:07:13 -0500

    con musica

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 onload = () => {
     document.body.classList.remove("container");
   };
+  
+function toggleMusic() {
+    var music = document.getElementById("background-music");
+    if (music.paused) {
+        music.play();
+    } else {
+        music.pause();
+    }
+}
